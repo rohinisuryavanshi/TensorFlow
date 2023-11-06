f0dbfd63da8d00a2f0fcfedbdab3fbaa245a182f
rohinisuryavanshi <rohini.suryavanshi@mhtechin.com>
2023-11-06 12:48:00 +0530

    second commit

--- tensorflow.py
+++ tensorflow.py
@@ -1,3 +1,7 @@
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
+
+# Load the MNIST dataset
+mnist = keras.datasets.mnist
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
